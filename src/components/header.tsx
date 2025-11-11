b8096cd55225719e505b88e50f76a798d5f96081
Hanjla <hanjla.muhamad@gmail.com>
2025-11-11 14:04:29 +0000

    make distance between logo and new emails button in mobile screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -68,7 +68,7 @@ export default function AppHeader({
 
   return (
     <header className="border-b p-3">
-      <div className="container mx-auto flex items-center justify-between gap-4">
+      <div className="container mx-auto flex items-center justify-between gap-2">
         <div className="flex items-center gap-3">
           <ShieldCheck className="w-8 h-8 text-primary" />
           <h1 className="text-2xl font-bold tracking-tighter">TempMail</h1>
